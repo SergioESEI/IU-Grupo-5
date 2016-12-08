b4c19dce0d707591f268befbae50c95cd8659048
asfarinhas <asfarinhas@esei.uvigo.es>
2016-12-08 18:05:06 +0100

    Add files via upload

---
+++ views/TRABAJADOR_ADD_Vista.php
@@ -0,0 +1,234 @@
+<?php
+
+//Si no hay una sesión iniciada, la inicia.
+if(!isset($_SESSION)){
+	session_start();
+}
+//Comprueba si el usuario inició sesión y si es admin antes de cargar la página.
+if(isset($_SESSION['grupo']) && strcmp($_SESSION['grupo'],"Admin") == 0 ){ 
+
+//Crea la clase e instancia la función render en el constructor. 
+	class Trabajador_Crear{
+
+		function __construct(){
+			$this->render();
+		}
+		function render(){
+			require_once('../header.php'); 
+			?>
+			<!-- Título de la página -->
+			<title><?php echo $strings['titulo añadir trabajador']; ?></title>
+			<script>
+		//Compruba que el formato del DNI sea válido.
+		function nif(dni) {
+			var numero
+			var letr
+			var letra
+			var expresion_regular_dni
+			
+			expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+			
+			if(expresion_regular_dni.test (dni) == true){
+				numero = dni.substr(0,dni.length-1);
+				letr = dni.substr(dni.length-1,1);
+				numero = numero % 23;
+				letra='TRWAGMYFPDXBNJZSQVHLCKET';
+				letra=letra.substring(numero,numero+1);
+				if (letra!=letr.toUpperCase()) {
+					alert('<?php echo $strings['error letra dni']; ?>');
+				}
+			}else{ 
+				if (dni != "")
+					alert('<?php echo $strings['error dni']; ?>');
+			}
+		}
+
+
+		function validateForm() {
+		    var nombre = document.forms["form"]["nombre"].value;
+		    if (x == "") {
+		        alert("Name must be filled out");
+        return false;
+    }
+}
+	</script>
+	<body>
+		<!-- Include del menú-->
+		<div class="row-fluid">
+			<?php include_once('menu.php'); ?>
+
+			<div class="col-sm-10 text-left">
+				<div class="section-fluid">
+					<div class="container-fluid">
+
+						
+						<!-- Formulario para añadir usuario -->
+						<form class="form-horizontal" role="form" action="../controllers/TRABAJADOR_Controller.php?id=altaTrabajador" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
+							<div class="form-group"  >
+								<div class="col-md-12"> <h2 class="text-info "><?php echo $strings['nuevo trabajador']; ?></h2></div>
+								<div class="col-md-12"><hr></div>
+
+								<!-- Campo DNI-->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['dni']; ?>:</label>
+									</div>
+									<div class="col-sm-4"> 
+										<input type="text" required class="form-control" name="dni" onblur="nif(this.value)">
+									</div>
+								</div>
+								
+								<!-- Campo Nombre -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['nombre']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="nombre"  title="<?php echo $strings['error trabajador']; ?>" >
+									</div>
+								</div>
+								
+
+								<!-- Campo Apellidos -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['apellidos']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="apellidos"  title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+								<!-- Campo Foto de perfil -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['fotoperfil'];?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input name="fotoperfil" type="file" class="control-label" title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+								<!-- Campo Direccion -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['direccion']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="direccion"  title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+								<!-- Campo Email -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['email']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+								<!-- Campo Fecha Nacimiento -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['fechanac']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input class = "tcal" type = "date" name="fechanac"  title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+
+								<!-- Campo Tipo de Empleado-->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['tipoemp']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<select name="tipoemp" required>
+											<option value="administrador"><?php echo $strings['administrador'];?></option>;
+											<option value="secretario"><?php echo $strings['secretario'];?></option>;
+											<option value="monitor"><?php echo $strings['monitor'];?></option>;
+											<option value="fisioterapeuta"><?php echo $strings['fisioterapeuta'];?></option>;
+											<option value="cafeteria"><?php echo $strings['cafeteria'];?></option>;
+											<option value="limpieza"><?php echo $strings['limpieza'];?></option>;
+											<option value="otros"><?php echo $strings['otros'];?></option>;
+										</select>
+									</div>
+								</div>
+								
+								
+								
+								<!-- Campo Observaciones -->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['observaciones']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<textarea name="observaciones" rows="10" cols="40"></textarea>
+									</div>
+								</div>
+
+
+
+								<!-- Campo Numero de Cuenta -->
+
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['numerocuenta']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="numerocuenta"  title="<?php echo $strings['error trabajador']; ?>" required>
+									</div>
+								</div>
+
+
+
+
+
+
+								
+								<!-- Campo Telefono -->
+								
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['telefono']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="text" class="form-control" name="telefono"  title="<?php echo $strings['error trabajador']; ?>">
+									</div>
+								</div>
+
+								<!-- Campo Externo-->
+								<div class="form-group">
+									<div class="col-sm-4">
+										<label for="nombre" class="control-label"><?php echo $strings['externo']; ?>:</label>
+									</div>
+									<div class="col-sm-4">
+										<input type="radio" name="externo" value="0" autofocus required="required">Externo
+										<br>
+										<input type="radio" name="externo" value="1">No externo
+									</div>
+								</div>
+								
+
+								<!-- Submit que envía los datos para crear el trabajador -->
+								<div class="form-group">
+									<div class="col-sm-4"></div>
+									<div class="col-sm-4"><input class="btn btn-primary" value="<?php echo $strings['crear']; ?>" type="submit">
+										<input class="btn btn-default" value="<?php echo $strings['resetear']; ?>" type="reset"></div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+		<?php 
+	}
+}
+}else
+echo "Permiso denegado.";
+?>
---
+++ views/TRABAJADOR_EDIT_CONFIRMAR_Vista.php
@@ -0,0 +1,216 @@
+<?php
+
+//Si no hay una sesión iniciada, la inicia.
+if(!isset($_SESSION)){
+	session_start();
+}
+//Comprueba si el usuario inició sesión y si es admin antes de cargar la página.
+if(isset($_SESSION['grupo']) && strcmp($_SESSION['grupo'],"Admin") == 0 ){ 
+
+//Crea la clase e instancia la función render en el constructor. 
+	class Trabajador_Editar_Confirmar{
+
+		function __construct($trabajador,$id2){
+			$this->render($trabajador,$id2);
+		}
+
+		function render($trabajador,$id2){
+			include_once('../header.php'); 
+			?>
+			<title><?php echo $strings['Confirmar trabajador']; ?></title>
+
+			<body>
+
+				<div class="row-fluid">
+					<!-- Include del menú -->
+					<?php include_once('menu.php'); ?>
+					<div class="col-sm-10 text-left">
+						<div class="section-fluid">
+							<div class="container-fluid">
+										
+										<form class="form-horizontal" role="form" action="../controllers/TRABAJADOR_Controller.php?id=modificarTrabajador&id2=<?php echo $id2;?>&confirm=ok" method="POST" enctype="multipart/form-data" >
+										<div class="form-group">
+
+											<div class="col-md-20"> <h2 class="text-info "><?php echo $strings['buscar trabajador']; ?></h2></div>
+											<div class="col-md-20"><hr></div>
+											<br>
+											<input type="hidden" name="dniN" value="<?php echo $_GET['id2']; ?>">
+											<input type="hidden" name="dni" value="<?php echo $_GET['id2']; ?>">
+
+											<!-- Campo DNI-->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['dni']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													
+													<input type="text" id="dni" class="form-control" name="dniN" value="<?php echo $trabajador->dni; ?>" readonly>
+													
+												</div>
+											</div>
+
+											<!-- Campo Nombre -->
+											<div class="form-horizontal">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['nombre']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<input type="text" class="form-control" name="nombreN" value="<?php echo $trabajador->nombre; ?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+												</div>
+											</div>
+											<br>
+											<br>
+											<br>
+
+											<!-- Campo Apellidos -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['apellidos']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<input type="text" class="form-control" name="apellidosN" value="<?php echo $trabajador->apellidos; ?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+												
+												</div>
+											</div>
+											<br>
+
+											<!-- Campo Foto de perfil -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['fotoperfil'];?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<img src="<?php echo $trabajador->url_Foto; ?>">
+												</div>
+											</div>
+
+
+											<!-- Campo Direccion -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['direccion']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+
+													<input type="text" class="form-control" name="direccionN" value="<?php echo $trabajador->direccion;?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+								
+												</div>
+											</div>
+
+											<!-- Campo Email -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['email']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<input type="text" class="form-control" name="emailN" value="<?php echo $trabajador->email;?>"  title="<?php echo $strings['error trabajador']; ?>" readonly>
+												</div>
+											</div>
+
+											<!-- Campo Fecha Nacimiento -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['fechanac']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<input type="text" class="form-control" name="fechanacN" value="<?php echo $trabajador->fechaNac;?>"  title="<?php echo $strings['error trabajador']; ?>" readonly>	
+												</div>
+											</div>
+
+											
+
+											<!-- Campo Tipo de Empleado-->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['tipoemp']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+												
+													<input type="text" class="form-control" name="tipoempN" value="<?php echo $trabajador->tipoEmp; ?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+												
+												</div>
+											</div>
+											
+											
+											
+											<!-- Campo Observaciones -->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['observaciones']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+											
+													<textarea name="observacionesN" readonly rows="10" cols="40"><?php echo $trabajador->observaciones; ?></textarea>
+												
+												</div>
+											</div>
+
+
+
+											<!-- Campo Numero de Cuenta -->
+
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['numerocuenta']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+										
+													<input type="text" class="form-control" name="numerocuentaN"  value="<?php echo $trabajador->numeroCuenta; ?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+												
+												</div>
+											</div>
+
+											<!-- Campo Telefono -->
+
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['telefono']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+										
+													<input type="text" class="form-control" name="telefonoN"  value="<?php echo $trabajador->telefono; ?>" title="<?php echo $strings['error trabajador']; ?>" readonly>
+												
+												</div>
+											</div>
+
+
+											<!-- Campo Externo-->
+											<div class="form-group">
+												<div class="col-sm-4">
+													<label for="nombre" class="control-label"><?php echo $strings['externo']; ?>:</label>
+												</div>
+												<div class="col-sm-4">
+													<?php 
+														if($trabajador->externo == 0){?>
+														<input type="radio" readonly name="externoN" value="0" disabled checked>Externo
+														<br>
+														<input type="radio" readonly name="externoN" value="1" disabled>No externo
+														<?php }else{?>
+														<input type="radio" readonly name="externoN" value="0" disabled>Externo
+														<br>
+														<input type="radio" readonly name="externoN" value="1" disabled checked>No externo
+														<?php }?>
+													
+												</div>
+											</div>
+											
+
+											<!-- Submit que envía los datos para crear el trabajador -->
+											<div class="form-group">
+												<div class="col-sm-4"></div>
+												<div class="col-sm-4">
+													<input class="btn btn-primary" value="<?php echo $strings['modificar']; ?>" type="submit">
+													<input type="button" class="btn btn-default" onclick="history.back()" name="volver atrás" value="volver atrás">
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</body>
+						<?php 
+					}
+				}
+			}else
+			echo "Permiso denegado.";
+			?>
