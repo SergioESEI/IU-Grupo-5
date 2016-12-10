23b98912e510f6a57341b65036f194817f812fb9
Iagosevic <micromachine13@hotmail.com>
2016-12-10 15:15:36 +0100

    Add files via upload

---
+++ models/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor = "localhost";
+	$base_datos = "moovett";
+	$usuario_bbdd = "moovett";
+	$clave_bbdd = "moovett2016";
+	
+	$conexion = mysqli_connect($servidor, $usuario_bbdd, $clave_bbdd,$base_datos);
+?>
